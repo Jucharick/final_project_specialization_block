c2f09797d5006e3909c1cac41be92459f83ecbe4
Julia Ch <juliecha@yandex.ru>
2023-04-26 10:03:34 +0300

    Add Notes_OOP

---
+++ Notes_OOP/Note.py
@@ -0,0 +1,5 @@
+class Note:
+    def __init__(self, id_note, name, body):
+            self.id = id_note
+            self.name = name
+            self.body = body
---
+++ Notes_OOP/controller.py
@@ -0,0 +1,24 @@
+def start():
+    value =''
+    while True:
+        # value = view.menu()
+        match value:
+            case 1:
+                # Показать список заметок
+                pass
+            case 2:
+                # Создать новую заметку
+                pass
+            case 3:
+                # Показать заметку
+                pass
+            case 4:
+                # Изменить
+                pass
+            case 5:
+                # Удалить
+                pass
+            case 6:
+                # Выход из программы
+                
+                break
